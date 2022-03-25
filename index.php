b49c5d69279a0c6661919120000c714941f20e78
feyzullah Demir <101548542+feyzullahdemir@users.noreply.github.com>
2022-03-25 10:47:31 +0300

    update Wordpress Hook

--- index.php
+++ index.php
@@ -12,30 +12,71 @@ Author URI: https:iyzico.com
 License: GNU
 */
 
+class IyzicoTaksitTag
+{
+	 protected static $instance;
+       
+        public static function getInstance() {
 
+            if (self::$instance === null ) {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+
+
+
+	
+
+
+public static function iyzicoActive(){
 global $wpdb;
 $charset = $wpdb->get_charset_collate();
 $tableName = $wpdb->prefix."taksit";
 $sql = "CREATE TABLE $tableName (
-id INT(55) NOT NULL AUTO_INCREMENT , 
+id INT(55) NOT NULL AUTO_INCREMENT ,
 İnstBank varchar(255) NOT NULL ,
-instNumber INT(55) NOT NULL , 
-instPrice INT(55) NOT NULL , 
-instStatus ENUM('0','1') NOT NULL , 
-UNIQUE KEY id (id) 
+instNumber INT(55) NOT NULL ,
+instPrice INT(55) NOT NULL ,
+instStatus ENUM('0','1') NOT NULL ,
+UNIQUE KEY id (id)
 ) $charset;";
 require_once (ABSPATH. "wp-admin/includes/upgrade.php");
 
 dbDelta($sql);
 
-register_activation_hook(__FILE__ , 'creating_plugin_table');
+
+}
+
+public static function iyziDeactivation(){
+
+
+
+            global $wpdb;
+            $tableName = $wpdb->prefix."taksit";
+            $charset = $wpdb->get_charset_collate();
+            
+
+            $sql = "DROP TABLE IF EXISTS $tableName;";
+            $wpdb->query($sql);
+           
+            flush_rewrite_rules();
+}
+
+
+
+}
+
+ 
+
+//Hook yapısına göre bir gelitiştme yapılmalı.
+IyzicoTaksitTag::getInstance();
+register_activation_hook(__FILE__ , 'iyzicoActive');
+register_deactivation_hook( __FILE__, 'iyziDeactivation' );
 
 
 require_once ('wpAdminTaksit.php');
-
-
-
-
 
 require_once ('wpProductTab.php');
 
